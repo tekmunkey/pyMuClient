1ec55cbcb57bbf2de597f23c0bde5878c80043ed
tekmunkey <gitid@tekmunkey.com>
2017-12-11 14:48:56 -0700

    ... and the cat came back the very next day, with tricks and treats, error handling and all manner of cross-platform optimizations in the user I/O loop, command processing, and the networkClient.

--- main.py
+++ main.py
@@ -11,79 +11,13 @@ print("Welcome to pyMuClient!  You may type /connect host:port to connect to a M
       "/exitclient to disconnect and exit immediately at any time.")
 
 #
-# A line of characters that may be passed on to client commands processing or networkClient transmission
-#
-chrline = [ ]
-#
 # The main app loop
 #
 while not appVariables.appExiting:
-    #
-    # This reads each character individually from stdin as the user types, and echoes it to the screen, while
-    # storing it for further processing
-    #
-    inChr = getch()
-    #
-    # This converts each character to its ordinal/numeric value for easier handling in if/else tests
-    #
-    inOrd = ord( inChr )
-    #
-    # testing if inChr is printable...
-    #
-    # I'm actually extremely confused as to how and why backspace works at all, since stdin.read doesn't actually
-    # return anything when I hit backspace.  There is literally no event where inOrd == 0x08
-    #
-    # HOWEVER... under Windows 7 with Python 2.7?  THIS WORKS JUST FINE.  It not only works fine in the console,
-    # it also updates into the chrline[] array even though I'm constantly doing appends and never actually doing
-    # any pops or removes at any point.  So.  Whiskey Tango Foxtrot?!
-    #
-    # I think, perhaps, that stdin.read doesn't actually return anything at all until or unless the user actually
-    # presses the return key, at which point stdin.read actually returns the entire line 1 character at a time,
-    # which is a little less handy for low-level operations (macroing) but perfectly serviceable anyway.
-    #
-    if (inOrd > 0x1f) and (inOrd < 0x7e):
-        chrline.append( inChr )
-        sys.stdout.write( inChr )
-        sys.stdout.flush()
-    # testing if inChr is enter key - using custom getch this may be 0d in Windows or 0a in unix
-    elif (inOrd == 0x0d) or (inOrd == 0x0a):
-        #
-        # Windows for some reason doubles-up on enter keypresses - the regular sys.stdin.read does a 0x0d-0x0a while msvcrt does
-        # a 0x0d-0x0d so we need to make sure we're not trapping 2 keypresses for no good reason
-        #   * We should probably just ignore the end-user if they want to hit enter on empty lines anyway, eh
-        #
-        if len( chrline ) > 0:
-            # get the full text
-            fullText = ''.join( chrline )
-            # break on line separator
-            sys.stdout.write( os.linesep )
-            sys.stdout.flush( )
-            #
-            # clear the chrline after fully processing it with the user's enter-keypress
-            #
-            chrline = [ ]
-            if not acp.processCommmand( fullText ):
-                # there may be some error handling sometime in the future if we ever make appCommandProcessor
-                # return a false value
-                pass
-    elif inOrd == 0x08:
-        if ( len( chrline ) > 0 ):
-            chrline.pop( len(chrline) - 1 )
-            #
-            # for each backspace keystroke we must instruct stdout to cursor backward, print one whitespace over the
-            # existing character, and backspace the cursor back into that location again
-            #
-            # sys.stdout.write("\b \b")
-            sys.stdout.write("\r")
-            sys.stdout.flush( )
-            sys.stdout.write(' ' * ( len( chrline ) + 1 ) )
-            sys.stdout.flush( )
-            sys.stdout.write( "\r" )
-            sys.stdout.flush( )
-            sys.stdout.write(''.join( chrline ))
-    #
-    # finally let the while continue
-    #
+    inStr = raw_input("").strip( "\n" ).strip( "\r" )
+    if not acp.processCommmand( inStr ):
+        # we may add some additional processing later if appCommandProcessor.processCommand ever returns false
+        pass
     continue
 
 nc.doDisconnect()
